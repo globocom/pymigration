a354ee51d3ee5a7af4f02cf3eeafdb23441915c5
nsigustavo <nsigustavo@gmail.com>
2013-02-27 14:55:43 -0300

    added display version

--- pymigration/model.py
+++ pymigration/model.py
@@ -1,3 +1,5 @@
+import os
+
 
 class Migrations(object):
 
@@ -6,3 +8,10 @@ class Migrations(object):
 
     def downgrade(self, version=0):
         print "Starting migration down!"
+
+
+
+
+def fullpath(*args):
+    project_path = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(project_path, '..', *args))
