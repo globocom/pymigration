acf26ec7eff18e7af492b594dc456fad3511c00f
SergioJorge <sergiojcamposjr@gmail.com>
2013-03-07 18:45:12 -0300

    Added test to method down in Migration

--- tests/unit/test_migration.py
+++ tests/unit/test_migration.py
@@ -11,12 +11,22 @@ from pymigrations import hello_world
 
 class TestMigration(unittest2.TestCase):
 
+    def setUp(self):
+        self.migration = Migration(hello_world)
+        self.original_stdout = sys.stdout
+        self.my_stdout = StringIO()
+        sys.stdout = self.my_stdout
+
     def test_should_execute_up_method_of_migration(self):
-        migration = Migration(hello_world)
-        original_stdout = sys.stdout
-        my_stdout = StringIO()
-        sys.stdout = my_stdout
-        migration.up()
-        output = my_stdout.getvalue()
-        sys.stdout = original_stdout
+        self.migration.up()
+        output = self.my_stdout.getvalue()
         self.assertEqual("HeLo World\n", output)
+
+    def test_should_execute_down_method_of_migration(self):
+        self.migration.down()
+        output = self.my_stdout.getvalue()
+        sys.stdout = self.original_stdout
+        self.assertEqual("Bye World\n", output)
+
+    def tearDown(self):
+        sys.stdout = self.original_stdout
